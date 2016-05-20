1e36a983dbc145656915e3b567d5af866f887ad8
jaehoon.jeong <ivory@wemakeprice.com>
2016-05-20 19:01:18 +0900

    Writing The UI for The Voting Screen

--- client/test/components/Voting_spec.jsx
+++ client/test/components/Voting_spec.jsx
@@ -10,7 +10,16 @@ import {expect} from 'chai';
 
 describe('Voting', () => {
 
-    // ...
+    it('renders a pair of buttons', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]} />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].textContent).to.equal('Trainspotting');
+        expect(buttons[1].textContent).to.equal('28 Days Later');
+    });
 
     it('invokes callback when a button is clicked', () => {
         let votedWith;
